b1f845b3a271b05068bbee7d79cb2f2e87c85fee
Gracie Lundell <gracielundell@gmail.com>
2017-10-13 23:25:14 -0700

    add logic to check if old value is greater or less than new value

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-console.log("slider: " + slider);
+// slider ==== output === value essentially; two ways to display same info
 output.innerHTML = slider.value; // Display the default slider value
 
 
@@ -44,11 +44,32 @@ slider.oninput = function() {
 
 
 // value from the slider then gets put into the applySaturation function //
+// have to store what the value was before it gets changed
+
+function onChangeTest(textbox) {
+	var newValue = textbox.value;
+	var oldValue = textbox.oldvalue;
+	console.log("Value is " + newValue);
+	console.log("Old Value is " + oldValue);
+
+	if (newValue < oldValue) {
+		oldValue -= newValue;
+		console.log("new value is less than than old value");
+	} else if (newValue > oldValue) {
+		console.log("new value is greater than old value");
+	} else {
+		console.log("new value is the same as old value");
+	}
 
 
-function applySaturation(value) {
-	Caman('#my-image', function() {
-		console.log(value);
-		this.saturation(value).render();
-	});
+	function applySaturation(newValue) {
+		Caman('#my-image', function() {
+			this.saturation(newValue).render();
+		});
+	}
+
 }
+
+
+
+// next: get saturation determined by slider to go up and down (math, if-statements) //
